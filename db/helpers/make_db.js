eca5617a423c47975b0458eee61d837259c45a5d
darlingson <dmakuwila@gmail.com>
2024-02-27 17:42:43 +0200

    added the *expenses table into the db, added the make_db helper file

---
+++ db/helpers/make_db.js
@@ -0,0 +1,6 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('db/expenses.db');
+
+db.run('CREATE TABLE expenses (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, amount REAL, category TEXT, date TEXT, note TEXT, created_at DATETIME DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,user_id TEXT)');
+db.close();
